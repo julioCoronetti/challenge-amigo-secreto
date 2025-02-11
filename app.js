dd6e2babaadddcdd9d1e0cc5bd5a751779c14317
Julio Coronetti <timeset13@outlook.com>
2025-02-11 14:14:53 -0300

    Add features using javascript

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let listaAmigos = document.querySelector("#listaAmigos");
+
+const adicionarAmigo = () => {
+    const amigo = document.querySelector("#amigo").value;
+    amigo == "" ? alert("Por favor, insira um nome.") : amigos.push(amigo);
+
+    adicionarNaTela(amigo, "mostrar");
+    limparCampo();
+}
+
+const limparCampo = () => {
+    document.querySelector("#amigo").value = "";
+}
+
+const sortearAmigo = () => {
+    try {
+        if (amigos.length == 0) {
+            throw new Error("Por favor, insira um nome.");
+        }
+
+        adicionarNaTela(amigo);
+        let resultado = document.querySelector("#resultado");
+        let li = document.createElement("li");
+        let random = parseInt(Math.random() * amigos.length + 1);
+        li.innerHTML = `O amigo sorteado é: <span>${amigos[random - 1]}</span>`;
+        resultado.appendChild(li);
+    } catch (erro) {
+        alert(erro);
+    }
+
+
+}
+
+const adicionarNaTela = (amigo, condicao) => {
+    if (condicao === "mostrar") {
+        let li = document.createElement("li");
+        li.innerHTML = amigo;
+        listaAmigos.appendChild(li);
+    } else {
+        listaAmigos.innerHTML = "";
+    }
+
+}
